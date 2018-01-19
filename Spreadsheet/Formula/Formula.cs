a9cb528398e952967ed0d8b355d54a2e7424eb42
Joseph Zachary <zachary@cs.utah.edu>
2018-01-19 14:17:41 -0700

    Add Formula and FormulaTest projects

---
+++ Spreadsheet/Formula/Formula.cs
@@ -0,0 +1,146 @@
+﻿// Skeleton written by Joe Zachary for CS 3500, January 2017
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Formulas
+{
+    /// <summary>
+    /// Represents formulas written in standard infix notation using standard precedence
+    /// rules.  Provides a means to evaluate Formulas.  Formulas can be composed of
+    /// non-negative floating-point numbers, variables, left and right parentheses, and
+    /// the four binary operator symbols +, -, *, and /.  (The unary operators + and -
+    /// are not allowed.)
+    /// </summary>
+    public class Formula
+    {
+        /// <summary>
+        /// Creates a Formula from a string that consists of a standard infix expression composed
+        /// from non-negative floating-point numbers (using C#-like syntax for double/int literals), 
+        /// variable symbols (a letter followed by zero or more letters and/or digits), left and right
+        /// parentheses, and the four binary operator symbols +, -, *, and /.  White space is
+        /// permitted between tokens, but is not required.
+        /// 
+        /// Examples of a valid parameter to this constructor are:
+        ///     "2.5e9 + x5 / 17"
+        ///     "(5 * 2) + 8"
+        ///     "x*y-2+35/9"
+        ///     
+        /// Examples of invalid parameters are:
+        ///     "_"
+        ///     "-5.3"
+        ///     "2 5 + 3"
+        /// 
+        /// If the formula is syntacticaly invalid, throws a FormulaFormatException with an 
+        /// explanatory Message.
+        /// </summary>
+        public Formula(String formula)
+        {
+        }
+        /// <summary>
+        /// Evaluates this Formula, using the Lookup delegate to determine the values of variables.  (The
+        /// delegate takes a variable name as a parameter and returns its value (if it has one) or throws
+        /// an UndefinedVariableException (otherwise).  Uses the standard precedence rules when doing the evaluation.
+        /// 
+        /// If no undefined variables or divisions by zero are encountered when evaluating 
+        /// this Formula, its value is returned.  Otherwise, throws a FormulaEvaluationException  
+        /// with an explanatory Message.
+        /// </summary>
+        public double Evaluate(Lookup lookup)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Given a formula, enumerates the tokens that compose it.  Tokens are left paren,
+        /// right paren, one of the four operator symbols, a string consisting of a letter followed by
+        /// zero or more digits and/or letters, a double literal, and anything that doesn't
+        /// match one of those patterns.  There are no empty tokens, and no token contains white space.
+        /// </summary>
+        private static IEnumerable<string> GetTokens(String formula)
+        {
+            // Patterns for individual tokens
+            String lpPattern = @"\(";
+            String rpPattern = @"\)";
+            String opPattern = @"[\+\-*/]";
+            String varPattern = @"[a-zA-Z][0-9a-zA-Z]*";
+            // PLEASE NOTE:  I have added white space to this regex to make it more readable.
+            // When the regex is used, it is necessary to include a parameter that says
+            // embedded white space should be ignored.  See below for an example of this.
+            String doublePattern = @"(?: \d+\.\d* | \d*\.\d+ | \d+ ) (?: e[\+-]?\d+)?";
+            String spacePattern = @"\s+";
+
+            // Overall pattern.  It contains embedded white space that must be ignored when
+            // it is used.  See below for an example of this.
+            String pattern = String.Format("({0}) | ({1}) | ({2}) | ({3}) | ({4}) | ({5})",
+                                            lpPattern, rpPattern, opPattern, varPattern, doublePattern, spacePattern);
+
+            // Enumerate matching tokens that don't consist solely of white space.
+            // PLEASE NOTE:  Notice the second parameter to Split, which says to ignore embedded white space
+            /// in the pattern.
+            foreach (String s in Regex.Split(formula, pattern, RegexOptions.IgnorePatternWhitespace))
+            {
+                if (!Regex.IsMatch(s, @"^\s*$", RegexOptions.Singleline))
+                {
+                    yield return s;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// A Lookup method is one that maps some strings to double values.  Given a string,
+    /// such a function can either return a double (meaning that the string maps to the
+    /// double) or throw an UndefinedVariableException (meaning that the string is unmapped 
+    /// to a value. Exactly how a Lookup method decides which strings map to doubles and which
+    /// don't is up to the implementation of the method.
+    /// </summary>
+    public delegate double Lookup(string var);
+
+    /// <summary>
+    /// Used to report that a Lookup delegate is unable to determine the value
+    /// of a variable.
+    /// </summary>
+    [Serializable]
+    public class UndefinedVariableException : Exception
+    {
+        /// <summary>
+        /// Constructs an UndefinedVariableException containing whose message is the
+        /// undefined variable.
+        /// </summary>
+        /// <param name="variable"></param>
+        public UndefinedVariableException(String variable)
+            : base(variable)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Used to report syntactic errors in the parameter to the Formula constructor.
+    /// </summary>
+    [Serializable]
+    public class FormulaFormatException : Exception
+    {
+        /// <summary>
+        /// Constructs a FormulaFormatException containing the explanatory message.
+        /// </summary>
+        public FormulaFormatException(String message) : base(message)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Used to report errors that occur when evaluating a Formula.
+    /// </summary>
+    [Serializable]
+    public class FormulaEvaluationException : Exception
+    {
+        /// <summary>
+        /// Constructs a FormulaEvaluationException containing the explanatory message.
+        /// </summary>
+        public FormulaEvaluationException(String message) : base(message)
+        {
+        }
+    }
+}
---
+++ Spreadsheet/FormulaSimpleTests/FormulaSimpleTests.cs
@@ -0,0 +1,127 @@
+﻿// Written by Joe Zachary for CS 3500, January 2017.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Formulas;
+
+namespace FormulaTestCases
+{
+    /// <summary>
+    /// These test cases are in no sense comprehensive!  They are intended to show you how
+    /// client code can make use of the Formula class, and to show you how to create your
+    /// own (which we strongly recommend).  To run them, pull down the Test menu and do
+    /// Run > All Tests.
+    /// </summary>
+    [TestClass]
+    public class UnitTests
+    {
+        /// <summary>
+        /// This tests that a syntactically incorrect parameter to Formula results
+        /// in a FormulaFormatException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct1()
+        {
+            Formula f = new Formula("_");
+        }
+
+        /// <summary>
+        /// This is another syntax error
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct2()
+        {
+            Formula f = new Formula("2++3");
+        }
+
+        /// <summary>
+        /// Another syntax error.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct3()
+        {
+            Formula f = new Formula("2 3");
+        }
+
+        /// <summary>
+        /// Makes sure that "2+3" evaluates to 5.  Since the Formula
+        /// contains no variables, the delegate passed in as the
+        /// parameter doesn't matter.  We are passing in one that
+        /// maps all variables to zero.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate1()
+        {
+            Formula f = new Formula("2+3");
+            Assert.AreEqual(f.Evaluate(v => 0), 5.0, 1e-6);
+        }
+
+        /// <summary>
+        /// The Formula consists of a single variable (x5).  The value of
+        /// the Formula depends on the value of x5, which is determined by
+        /// the delegate passed to Evaluate.  Since this delegate maps all
+        /// variables to 22.5, the return value should be 22.5.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate2()
+        {
+            Formula f = new Formula("x5");
+            Assert.AreEqual(f.Evaluate(v => 22.5), 22.5, 1e-6);
+        }
+
+        /// <summary>
+        /// Here, the delegate passed to Evaluate always throws a
+        /// UndefinedVariableException (meaning that no variables have
+        /// values).  The test case checks that the result of
+        /// evaluating the Formula is a FormulaEvaluationException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate3()
+        {
+            Formula f = new Formula("x + y");
+            f.Evaluate(v => { throw new UndefinedVariableException(v); });
+        }
+
+        /// <summary>
+        /// The delegate passed to Evaluate is defined below.  We check
+        /// that evaluating the formula returns in 10.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate4()
+        {
+            Formula f = new Formula("x + y");
+            Assert.AreEqual(f.Evaluate(Lookup4), 10.0, 1e-6);
+        }
+
+        /// <summary>
+        /// This uses one of each kind of token.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate5 ()
+        {
+            Formula f = new Formula("(x + y) * (z / x) * 1.0");
+            Assert.AreEqual(f.Evaluate(Lookup4), 20.0, 1e-6);
+        }
+
+        /// <summary>
+        /// A Lookup method that maps x to 4.0, y to 6.0, and z to 8.0.
+        /// All other variables result in an UndefinedVariableException.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public double Lookup4(String v)
+        {
+            switch (v)
+            {
+                case "x": return 4.0;
+                case "y": return 6.0;
+                case "z": return 8.0;
+                default: throw new UndefinedVariableException(v);
+            }
+        }
+    }
+}
