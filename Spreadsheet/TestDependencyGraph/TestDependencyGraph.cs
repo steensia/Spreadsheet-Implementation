165d2639ca078426e5d1279ba242edb4fa9f167d
Steen Sia <steensia101@gmail.com>
2018-01-28 16:29:30 -0700

    Added test cases

--- Spreadsheet/TestDependencyGraph/TestDependencyGraph.cs
+++ Spreadsheet/TestDependencyGraph/TestDependencyGraph.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dependencies;
+
 
 namespace TestDependencyGraph
 {
@@ -11,12 +13,146 @@ namespace TestDependencyGraph
     public class TestDependencyGraph
     {
         /// <summary>
-        /// 
+        /// Check if HasDependents throws a null exception
         /// </summary>
         [TestMethod]
-        public void CheckNullHasDependents()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void HasDependentsCheckNull()
         {
-            DependencyGraph ok = new DependencyGraph();
+            DependencyGraph graph = new DependencyGraph();
+            graph.HasDependents(null);
+        }
+
+        /// <summary>
+        /// Check if HasDependees throws a null exception
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void HasDependeesCheckNull()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.HasDependees(null);
+        }
+
+        /// <summary>
+        /// Check if GetDependents throws a null exception
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDependentsCheckNull()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.GetDependents(null);
+        }
+
+        /// <summary>
+        /// Check if GetDependees throws a null exception
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetDependeesCheckNull()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.GetDependees(null);
+        }
+
+        /// <summary>
+        /// Check if AddDependency throws a null exception to first argument
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddDependencyCheckNull1()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.AddDependency(null, "DG");
+        }
+
+        /// <summary>
+        /// Check if AddDependency throws a null exception to second argument
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddDependencyCheckNull2()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.AddDependency("DG", null);
+        }
+
+        /// <summary>
+        /// Check if AddDependency throws a null exception to both arguments
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddDependencyCheckNull3()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.AddDependency(null, null);
+        }
+
+        /// <summary>
+        /// Check if AddDependency deals with a duplicate dependency
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddDependencyDuplicate()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.AddDependency("s", "t");
+            graph.AddDependency("s", "t");
+        }
+
+        /// <summary>
+        /// Check if RemoveDependency throws a null exception to first argument
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDependencyCheckNull1()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.RemoveDependency(null, "DG");
+        }
+
+        /// <summary>
+        /// Check if RemoveDependency throws a null exception to second argument
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDependencyCheckNull2()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.RemoveDependency("DG", null);
+        }
+
+        /// <summary>
+        /// Check if RemoveDependency throws a null exception to both arguments
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDependencyCheckNull3()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.RemoveDependency(null, null);
+        }
+
+        /// <summary>
+        /// Check if RemoveDependency deals with non-existent dependency
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDependencyInvalidDependency()
+        {
+            DependencyGraph graph = new DependencyGraph();
+            graph.RemoveDependency("s", "t");
+        }
+
+        /// <summary>
+        /// Check if RemoveDependents deals throw null exception to first argument
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDependentsNull1()
+        {
+            DependencyGraph graph = new DependencyGraph();
         }
     }
 }
