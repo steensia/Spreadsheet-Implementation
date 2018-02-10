548a7c5cbb8f3fe970c202894fec1980e9991715
Steen Sia <steensia101@gmail.com>
2018-02-10 00:40:44 -0700

    Started on PS5, added Unit Tests

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dependencies;
+using Formulas;
+
+namespace SS
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Spreadsheet()
+        {
+        }
+
+        /// <summary>
+        /// Enumerates the names of all the non-empty cells in the spreadsheet.
+        /// </summary>
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
+        /// value should be either a string, a double, or a Formula.
+        /// </summary>
+        public override object GetCellContents(string name)
+        {
+            if (name == null)
+            {
+                throw new InvalidNameException();
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes number.  The method returns a
+        /// set consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            if (name == null)
+            {
+                throw new InvalidNameException();
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If text is null, throws an ArgumentNullException.
+        /// 
+        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes text.  The method returns a
+        /// set consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (name == null)
+            {
+                throw new InvalidNameException();
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Requires that all of the variables in formula are valid cell names.
+        /// 
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
+        /// circular dependency, throws a CircularException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes formula.  The method returns a
+        /// Set consisting of name plus the names of all other cells whose value depends,
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            if (name == null)
+            {
+                throw new InvalidNameException();
+            }
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If name is null, throws an ArgumentNullException.
+        /// 
+        /// Otherwise, if name isn't a valid cell name, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns an enumeration, without duplicates, of the names of all cells whose
+        /// values depend directly on the value of the named cell.  In other words, returns
+        /// an enumeration, without duplicates, of the names of all cells that contain
+        /// formulas containing name.
+        /// 
+        /// For example, suppose that
+        /// A1 contains 3
+        /// B1 contains the formula A1 * A1
+        /// C1 contains the formula B1 + A1
+        /// D1 contains the formula B1 - C1
+        /// The direct dependents of A1 are B1 and C1
+        /// </summary>
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            if (name == null)
+            {
+                throw new InvalidNameException();
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class SpreadsheetTests
+    {
+        /// <summary>
+        /// Check if GetCellContents throws an InvalidNameException when name is null or invalid
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void GetCellContentsNullCheck()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.GetCellContents(null);
+        }
+
+        /// <summary>
+        /// Check if SetCellContents throws an InvalidNameException when name is null or invalid
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SetCellContentsNullCheck()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents(null, 20);
+        }
+
+        /// <summary>
+        /// Check if SetCellContents throws an ArgumentNullException when text is null
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetCellContentsNullCheck2()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("x", null);
+        }
+
+        /// <summary>
+        /// Check if SetCellContents throws an InvalidNameException when name is null or invalid
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SetCellContentsNullCheck3()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents(null, "x");
+        }
+    }
+}
