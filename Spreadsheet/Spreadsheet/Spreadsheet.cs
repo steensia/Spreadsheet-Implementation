a4ea124fc79fc2b833e478db3c4d65a46307cd86
Steen Sia <steensia101@gmail.com>
2018-02-15 11:15:55 -0700

    Added constructor comments

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -9,7 +9,46 @@ using Formulas;
 namespace SS
 {
     /// <summary>
+    /// An AbstractSpreadsheet object represents the state of a simple spreadsheet.  A 
+    /// spreadsheet consists of an infinite number of named cells.
     /// 
+    /// A string s is a valid cell name if and only if it consists of one or more letters, 
+    /// followed by a non-zero digit, followed by zero or more digits.
+    /// 
+    /// For example, "A15", "a15", "XY32", and "BC7" are valid cell names.  On the other hand, 
+    /// "Z", "X07", and "hello" are not valid cell names.
+    /// 
+    /// A spreadsheet contains a cell corresponding to every possible cell name.  
+    /// In addition to a name, each cell has a contents and a value.  The distinction is
+    /// important, and it is important that you understand the distinction and use
+    /// the right term when writing code, writing comments, and asking questions.
+    /// 
+    /// The contents of a cell can be (1) a string, (2) a double, or (3) a Formula.  If the
+    /// contents is an empty string, we say that the cell is empty.  (By analogy, the contents
+    /// of a cell in Excel is what is displayed on the editing line when the cell is selected.)
+    /// 
+    /// In an empty spreadsheet, the contents of every cell is the empty string.
+    ///  
+    /// The value of a cell can be (1) a string, (2) a double, or (3) a FormulaError.  
+    /// (By analogy, the value of an Excel cell is what is displayed in that cell's position
+    /// in the grid.)
+    /// 
+    /// If a cell's contents is a string, its value is that string.
+    /// 
+    /// If a cell's contents is a double, its value is that double.
+    /// 
+    /// If a cell's contents is a Formula, its value is either a double or a FormulaError.
+    /// The value of a Formula, of course, can depend on the values of variables.  The value 
+    /// of a Formula variable is the value of the spreadsheet cell it names (if that cell's 
+    /// value is a double) or is undefined (otherwise).  If a Formula depends on an undefined
+    /// variable or on a division by zero, its value is a FormulaError.  Otherwise, its value
+    /// is a double, as specified in Formula.Evaluate.
+    /// 
+    /// Spreadsheets are never allowed to contain a combination of Formulas that establish
+    /// a circular dependency.  A circular dependency exists when a cell depends on itself.
+    /// For example, suppose that A1 contains B1*2, B1 contains C1*2, and C1 contains A1*2.
+    /// A1 depends on B1, which depends on C1, which depends on A1.  That's a circular
+    /// dependency.
     /// </summary>
     public class Spreadsheet : AbstractSpreadsheet
     {
